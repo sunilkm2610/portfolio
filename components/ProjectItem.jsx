51ecc8fb80c59064e8911c23f06649cd698f2c94
sunilflexday <sunil.kumar1@weareflexday.com>
2023-02-15 13:07:18 +0530

    fix in style

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -16,29 +16,31 @@ const ProjectItem = ({
         src={backgroundImg}
         alt="/"
       />
-      <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+      <div className="mx-auto w-full hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
         <h3 className="text-2xl text-white tracking-wider text-center">
           {title}
         </h3>
         <p className="pb-4 pt-2 text-white text-center">
           {tech}
         </p>
-        <a
-          href={codeURL}
-          target="_blank"
-          rel="noreferrer">
-          <button className="px-8 py-2 mt-4 mr-8">
-            Code
-          </button>
-        </a>
-        <a
-          href={demoURL}
-          target="_blank"
-          rel="noreferrer">
-          <button className="px-8 py-2 mt-4">
-            Demo
-          </button>
-        </a>
+        <div className="text-center">
+          <a
+            href={codeURL}
+            target="_blank"
+            rel="noreferrer">
+            <button className="px-8 py-2 mt-4 mr-4 mx-auto">
+              Code
+            </button>
+          </a>
+          <a
+            href={demoURL}
+            target="_blank"
+            rel="noreferrer">
+            <button className="px-8 py-2 mt-4">
+              Demo
+            </button>
+          </a>
+        </div>
       </div>
     </div>
   );
